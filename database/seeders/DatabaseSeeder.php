e0875f57b941679c0728bfa10575bdb397badf63
Vincent <msvincentius@gmail.com>
2024-08-16 09:09:43 +0700

    Adjust db, seeder (belom revamp)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Create a default admin user
+        User::create([
+            'uuid' => '1',
+            'name' => 'Vincent',
+            'nis' => '12209460',
+            'rayon' => 'Wikrama',
+            'position' => 'admin',
+            'username' => 'vinser',
+            'password' => Hash::make('password'),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Create a few test users
+        User::factory()->count(5)->create();
     }
 }
